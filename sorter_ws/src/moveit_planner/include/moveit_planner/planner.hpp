2482b0ec35375e7f38f233d7ff563fa54c11b561
Julian Britton <julian.m.britton@gmail.com>
2025-10-19 01:33:34 +1100

    added config folder, and added surrounding collision planes.

--- sorter_ws/src/moveit_planner/include/moveit_planner/planner.hpp
+++ sorter_ws/src/moveit_planner/include/moveit_planner/planner.hpp
@@ -9,11 +9,13 @@
 #include "tf2_ros/buffer.h"
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
+#include <moveit_msgs/msg/planning_scene.h>
 
 
 class Planner : public rclcpp::Node {
     public:
-        Planner();        
+        Planner();
 
 
 
@@ -22,7 +24,9 @@ class Planner : public rclcpp::Node {
             float sx,float sy, float sz, float x, float y, float z, std::string frame_id, std::string id);
 
 
+        void objectDetectedCallback(); // Add or modify collision objects for detected objects
+        void generatePathCallback(); // Generate and execute the path
 
-
+        std::shared_ptr<planning_scene_monitor::PlanningSceneMonitor> planning_scene_monitor;
         std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_interface;
 };
--- sorter_ws/src/moveit_planner/src/planner.cpp
+++ sorter_ws/src/moveit_planner/src/planner.cpp
@@ -5,20 +5,23 @@ Planner::Planner() : Node("planner") {
     move_group_interface = std::make_unique<moveit::planning_interface::MoveGroupInterface>(std::shared_ptr<rclcpp::Node>(this), "ur_manipulator");
     move_group_interface->setPlanningTime(10.0);
 
-    RCLCPP_INFO(this->get_logger(), "Grabbing the planning frame");
-    std::string frame_id = move_group_interface->getPlanningFrame();
-    RCLCPP_INFO(this->get_logger(), frame_id.c_str());
+    planning_scene_monitor = std::make_unique<planning_scene_monitor::PlanningSceneMonitor>(std::shared_ptr<rclcpp::Node>(this), "robot_description");
 
+    std::string frame_id = move_group_interface->getPlanningFrame();
 
     std::vector<moveit_msgs::msg::CollisionObject> collision_objects = {
         generateCollisionObject(2.4, 0.04, 1.0, 0.85, -0.30, 0.5, frame_id, "backWall"),
         generateCollisionObject(0.04, 1.2, 1.0, -0.30, 0.25, 0.5, frame_id, "sideWall"),
-        generateCollisionObject(2.4, 1.2, 0.04, 0.85, 0.0, 0.05, frame_id, "table"),
+        generateCollisionObject(2.4, 1.2, 0.04, 0.85, 0.25, -0.02, frame_id, "table"),
     };
-
+    
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
     planning_scene_interface.applyCollisionObjects(collision_objects);
+
+    // Setup allowed collisions
+    planning_scene_monitor::LockedPlanningSceneRW planning_scene(planning_scene_monitor);
+    auto& acm = planning_scene->getAllowedCollisionMatrixNonConst();
+    acm.setEntry("table", "base_link_inertia", true);
 }
 
 
