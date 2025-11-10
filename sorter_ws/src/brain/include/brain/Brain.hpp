b13dfcfd5956d647e96e98c37371239c3311dba8
Julian Britton <julian.m.britton@gmail.com>
2025-11-10 18:11:10 +1100

    Added dynamic updates to brain, will send 1 move request at a time instead of 2

--- sorter_ws/src/brain/include/brain/Brain.hpp
+++ sorter_ws/src/brain/include/brain/Brain.hpp
@@ -1,6 +1,8 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <queue>
+#include <chrono>
 
 
 #include "rclcpp/rclcpp.hpp"
@@ -12,13 +14,21 @@
 #include "geometry_msgs/msg/pose_stamped.hpp"
 
 
+struct ItemPose {
+    geometry_msgs::msg::Pose pose;
+    bool in_queue;
+
+    ItemPose(geometry_msgs::msg::Pose pose)
+        : in_queue(false), pose(pose) {}
+};
+
+
 class Brain : public rclcpp::Node {
 public:
     Brain();
 
     // Debugging function
-    void send_move_request(const geometry_msgs::msg::Pose &start_pose,
-                           const geometry_msgs::msg::Pose &goal_pose);
+    void send_move_request(const geometry_msgs::msg::Pose &pose);
 
 private:
     void item_topic_callback(const interfaces::msg::LabelledPoseArray &msg);
@@ -26,17 +36,22 @@ private:
     void goal_topic_callback(const interfaces::msg::LabelledPoseArray &msg);
 
     void send_move_request(const std::string &label);
-    
-    void move_request_response(const std::string &label, 
-        rclcpp::Client<interfaces::srv::Move>::SharedFuture future);
-        
-    // Function for debugging
-    void move_request_response(rclcpp::Client<interfaces::srv::Move>::SharedFuture future);
 
-    std::map<std::string, geometry_msgs::msg::Pose> item_pose_map;
+    bool move_request_response(rclcpp::Client<interfaces::srv::Move>::SharedFuture future);
+
+    inline void publish_goal_pose(const std::string &label);
+
+    inline void publish_item_pose(const std::string &label);
+
+    std::string get_goal_label(const std::string &item_label);
+        
+
+    std::queue<std::string> item_queue;
+    std::map<std::string, ItemPose> item_pose_map;
     std::map<std::string, geometry_msgs::msg::PoseStamped> goal_pose_map;
 
     rclcpp::Client<interfaces::srv::Move>::SharedPtr move_client;
+    rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pose_update_publisher;
     rclcpp::Subscription<interfaces::msg::LabelledPoseArray>::SharedPtr item_pose_subscription;
     rclcpp::Subscription<interfaces::msg::LabelledPoseArray>::SharedPtr goal_pose_subscription;
 };
