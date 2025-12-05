babe79b8dc0ca42cfef08c9c3451175c948e45cc
Julian Britton <julian.m.britton@gmail.com>
2025-12-05 15:00:39 +1100

    done instructions

---
+++ sorter_ws/src/sys_viz/launch/auxiliary_sim.launch.py
@@ -0,0 +1,39 @@
+import launch
+
+from launch_ros.actions import Node, ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+from launch.substitutions import PathJoinSubstitution, Command, FindExecutable
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # start brain node
+    brain_node = Node(
+        package="brain",
+        executable="brain",
+        name="brain"
+    )
+    arduino_bridge_node = Node(
+        package="teensy_pkg",
+        executable="util_arduino_node",
+        name="util_arduino_node",
+    )
+    transform_node = Node(
+        package="transforms",
+        executable="transform_node",
+        name="transform_node",
+    )
+    item_detector_node = Node(
+        package="perception",
+        executable="ItemDetector",
+        name="test-detector",
+    )
+
+
+
+    return launch.LaunchDescription([
+        brain_node,
+        arduino_bridge_node,
+        transform_node,
+        item_detector_node
+    ])
